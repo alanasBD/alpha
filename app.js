501dbb448486833ce4045d1b3a0dabf9ef111b56
al-anas <alanas7171@gmail.com>
2022-10-02 00:40:04 +0600

    used named function

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const exp = require("constants");
 const express = require("express");
 const db = require("./db");
 
-
 const app = express();
 
 app.use(express.json());
@@ -11,70 +10,69 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.get("/api/students", (req, res) => {
+//using named function
+const studentList = (req, res) => {
   db.getDbStudents().then((students) => {
     res.send(students);
   });
-});
+};
 
-app.post("/api/students", (req, res) => {
+const newStudent = (req, res) => {
   const student = req.body;
-
   db.getDbStudents().then((students) => {
     students.push(student);
-      db.insertDbStudent(students)
-      .then(data =>{
-         res.send(data)
-      })
+    db.insertDbStudent(students).then((data) => {
+      res.send(data);
+    });
   });
-});
+};
 
-app.get('/api/students/:id',(req,res)=>{
-   const id = parseInt(req.params.id);
-   db.getDbStudents()
-   .then(students =>{
-      const student = students.find(s => s.id ===id)
-      if(!student) res.status(404).send('No student found with this id')
-      else res.send(student);
-   })
-})
+const studentDetail = (req, res) => {
+  const id = parseInt(req.params.id);
+  db.getDbStudents().then((students) => {
+    const student = students.find((s) => s.id === id);
+    if (!student) res.status(404).send("No student found with this id");
+    else res.send(student);
+  });
+};
 
-app.put('/api/students/:id',(req,res)=>{
-   const id = parseInt(req.params.id);
-   const updatedInfo = req.body; 
-   db.getDbStudents()
-   .then(students=>{
-     const student = students.find(s => s.id === id)
-     if(!student) res.status(404).send('No student found with this id');
-     else{
-        const i = students.findIndex(s=>s.id === id)
-        console.log('i',i);
-        students[i] = updatedInfo;
-        db.insertDbStudent(students)
-        .then(msg =>{
-           res.send(msg);
-        });
-     }
-   })
-})
+const studentUpdate = (req, res) => {
+  const id = parseInt(req.params.id);
+  const updatedInfo = req.body;
+  db.getDbStudents().then((students) => {
+    const student = students.find((s) => s.id === id);
+    if (!student) res.status(404).send("No student found with this id");
+    else {
+      const i = students.findIndex((s) => s.id === id);
+      console.log("i", i);
+      students[i] = updatedInfo;
+      db.insertDbStudent(students).then((msg) => {
+        res.send(msg);
+      });
+    }
+  });
+};
 
-app.delete('/api/students/:id',(req,res)=>{
-   const id = parseInt(req.params.id);
+const studentDelete = (req, res) => {
+  const id = parseInt(req.params.id);
 
-   db.getDbStudents()
-   .then(students=>{
-      const student = students.find(s => s.id === id);
-      if(!student) res.status(404).send('No student found with this id');
+  db.getDbStudents().then((students) => {
+    const student = students.find((s) => s.id === id);
+    if (!student) res.status(404).send("No student found with this id");
 
-      const updatedStudents = students.filter(s => s.id !== id);
+    const updatedStudents = students.filter((s) => s.id !== id);
 
-      db.insertDbStudent(updatedStudents)
-      .then(msg =>{
-         res.send(student)
-      })
-   })
+    db.insertDbStudent(updatedStudents).then((msg) => {
+      res.send(student);
+    });
+  });
+};
 
-})
+app.get("/api/students", studentList);
+app.post("/api/students", newStudent);
+app.get("/api/students/:id", studentDetail);
+app.put("/api/students/:id", studentUpdate);
+app.delete("/api/students/:id", studentDelete);
 
 const PORT = 3000;
 app.listen(PORT, () => {
