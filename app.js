071ef0875f41d67441c1cdfacc26808b4f1cc7e9
al-anas <alanas7171@gmail.com>
2022-10-02 00:27:54 +0600

    put and delete methods implemented

--- app.js
+++ app.js
@@ -39,6 +39,43 @@ app.get('/api/students/:id',(req,res)=>{
    })
 })
 
+app.put('/api/students/:id',(req,res)=>{
+   const id = parseInt(req.params.id);
+   const updatedInfo = req.body; 
+   db.getDbStudents()
+   .then(students=>{
+     const student = students.find(s => s.id === id)
+     if(!student) res.status(404).send('No student found with this id');
+     else{
+        const i = students.findIndex(s=>s.id === id)
+        console.log('i',i);
+        students[i] = updatedInfo;
+        db.insertDbStudent(students)
+        .then(msg =>{
+           res.send(msg);
+        });
+     }
+   })
+})
+
+app.delete('/api/students/:id',(req,res)=>{
+   const id = parseInt(req.params.id);
+
+   db.getDbStudents()
+   .then(students=>{
+      const student = students.find(s => s.id === id);
+      if(!student) res.status(404).send('No student found with this id');
+
+      const updatedStudents = students.filter(s => s.id !== id);
+
+      db.insertDbStudent(updatedStudents)
+      .then(msg =>{
+         res.send(student)
+      })
+   })
+
+})
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log("Server is running...");
